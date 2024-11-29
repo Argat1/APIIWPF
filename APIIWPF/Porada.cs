1cbc2ded77ba1ed4732ff0ecb5b5fed45dff7193
lukasz.jablonski <lukasz.jablonski@S50-13>
2024-11-29 09:52:35 +0100

    Dodaj pliki projektów.

---
+++ APIIWPF/Porada.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIIWPF
+{
+    public static class Porada
+    {
+        public static async Task<string> LosuPorade()
+        {
+            string apiUrl = "https://api,adviceslip.com/advice";
+            using (HttpClient klient = new HttpClient())
+            {
+                HttpResponseMessage odpowiedz = await klient.GetAsync(apiUrl);
+                odpowiedz.EnsureSuccessStatusCode();
+                string odp = await odpowiedz.Content.ReadAsStringAsync();
+                var json = JObject.Parse(odp);
+                string porada = json["slip"]["advice"].ToString();
+                return porada;
+            }
+    }
+}
